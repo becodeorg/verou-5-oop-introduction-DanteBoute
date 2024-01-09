f95a67fcae73080b64a4d1d02a449c006bcd4253
DanteBoute <danteboute1@outlook.com>
2024-01-09 10:50:57 +0100

    Finish ex.4

--- exercise_4_protected.php
+++ exercise_4_protected.php
@@ -11,3 +11,55 @@ TODO: Make all the other prints work without error without changing the beverage
 
 USE TYPEHINTING EVERYWHERE!
 */
+
+class Beverage {
+    protected $color;
+    protected $price;
+    protected $temperature = 'cold';
+    protected $name;
+
+    public function __construct(string $name, string $color, float $price, string $temperature = 'cold') {
+        $this->name = $name;
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = $temperature;
+    }
+
+    public function getInfo() {
+        echo "This beverage is $this->temperature and $this->color.";
+    }
+
+    public function getColor() {
+        return $this->color;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+}
+
+class Beer extends Beverage {
+    protected $alcoholPercentage;
+
+    public function __construct(string $name, string $color, float $alcoholPercentage, float $price) {
+        parent::__construct($name, $color, 0, 'cold');
+        $this->alcoholPercentage = $alcoholPercentage;
+        $this->price = $price;
+    }
+
+    public function getAlcoholPercentage() {
+        return $this->alcoholPercentage;
+    }
+
+    public function getInfo() {
+        echo "This beer is a {$this->getName()} with alcohol percentage {$this->getAlcoholPercentage()}, the price of this beer is €{$this->getPrice()}.<br>";
+        echo "Color: " . $this->getColor() . "<br>";
+    }
+}
+
+$Duvel = new Beer("Duvel", "blond", 8.5, 3.5);
+echo $Duvel->getInfo();
